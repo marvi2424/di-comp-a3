8293219033adef3fe549eb81bf68b72262a6bc90
EmamulxEmon <emamulhemon@gmail.com>
2026-06-27 00:23:49 +0200

    Update handler.py

--- src/lambdas/sentiment/handler.py
+++ src/lambdas/sentiment/handler.py
@@ -1,1 +1,9 @@
 
+import boto3, os, json
+
+ssm = boto3.client('ssm', endpoint_url=os.environ.get('MINISTACK_ENDPOINT', 'http://localhost:4566'))
+
+def handler(event, context):
+    table = ssm.get_parameter(Name='/dic-reviews/dynamodb-table')['Parameter']['Value']
+    # TODO: P3 fills sentiment logic here
+    return {"statusCode": 200, "body": "sentiment skeleton ok"}
