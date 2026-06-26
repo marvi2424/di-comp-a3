928345ded798793946ed636be44e53430e67a8c3
EmamulxEmon <emamulhemon@gmail.com>
2026-06-27 00:22:43 +0200

    Update handler.py

--- src/lambdas/preprocessing/handler.py
+++ src/lambdas/preprocessing/handler.py
@@ -1,1 +1,9 @@
 
+import boto3, os, json
+
+ssm = boto3.client('ssm', endpoint_url=os.environ.get('MINISTACK_ENDPOINT', 'http://localhost:4566'))
+
+def handler(event, context):
+    bucket = ssm.get_parameter(Name='/dic-reviews/input-bucket')['Parameter']['Value']
+    # TODO: P2 fills preprocessing logic here
+    return {"statusCode": 200, "body": "preprocessing skeleton ok"}
